d0e99ee2ee46763f87a89d2173845471949ded30
SuuGarr <benchaphorn234@gmail.com>
2024-11-25 21:40:33 +0700

    change size

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts,scss}", 
   ],
   theme: {
+    screens: {
+      'mo': '414px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       margin: {
         '270px': '270px',
